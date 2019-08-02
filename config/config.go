e1ea3495151bd32ac116b8decd46922e549194c8
Dimitri Rusin <dimitri.rusin@rwth-aachen.de>
2019-08-02 20:09:15 +0200

    config: Add String

--- config/config.go
+++ config/config.go
@@ -1,18 +1,21 @@
 package config
 
 var (
-	Float = map[string]float32{
+	Float = map[string]float32 {
 		"komi": 5.5}
 
-	Int = map[string]int{
+	Int = map[string]int {
 		"boardsize": 5,
 		"max_game_length": 50,
 		"exploration_length": 3,
 		"predict_batch_size": 2,
 		"num_examples_per_file": 14,
-		"nsims": 600,
+		"nsims_per_goroutine": 6,
 		"random_seed": 3,
 		"history_size": 4}
+
+	String = map[string]string {
+		"exp_prefix": "exp"}
 )
 
 const (
